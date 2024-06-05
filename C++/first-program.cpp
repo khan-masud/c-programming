2c3a8ee25ca146022c6d319b8cc8db8c5ab6bc66
Masud <abdullahalmasudkhan@gmail.com>
2024-06-06 01:02:28 +0600

    Removed my all emotions from this code

--- C++/first-program.cpp
+++ C++/first-program.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main () {
 cout << "Abdullah Al Masud"; // Print String
-cout << "Moriom" << "Nesa" << "Labonna"; // Print string in different way.
+cout << "The" << "Offline" << "Dreamer"; // Print string in different way.
 
 // Lets decorate it.
 
 cout << "\nAbdullah Al Masud \n"; // New line
-cout << "Moriom" << " Nesa " << "Labonna\n"; // Space will print into the name.
+cout << "The" << " Offline " << "Dreamer\n"; // Space will print into the name.
 }
--- C++/variable.cpp
+++ C++/variable.cpp
@@ -7,24 +7,24 @@ int main ()
     Invalid Variable Name Format Example :
     --------------------------------------------
        1. 007masud
-       2. masud@gs, labonna#
+       2. masud@gs, dreamer#
        3. float, int, double, char etc (all keywords of c/c++)
-       4. masud-labonna
+       4. masud-dreamer
 
     Valid Variable Name Format Example :
-        1. Masud, labonna
-        2. masud_labonna
-        3. masudLabonna
-        4. Masud$Labonna
+        1. Masud, dreamer
+        2. masud_dreamer
+        3. masudDreamer
+        4. Masud$Dreamer
         5. Float, Char, Int, Double
-        6. _masud, _labonna_
+        6. _masud, _dreamer_
 
     */
     int age = 21; // Integer
     float gpa = 5.00; // Float
     double cgpa = 3.99; // Double
-    char letter = 'L'; // Character
-    char myQueen[] = "Labonna"; // String
+    char letter = 'N'; // Character
+    char myQueen[] = "Tor kono Queen nai bhai"; // String
 
     // Lets print them
     cout << "My age : " << age << "\n";
